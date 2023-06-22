f6cb780c2f8a988fbfe92c3937d77ee426c9971d
Salim Zaidi <121177411+zedsalim@users.noreply.github.com>
2023-06-22 12:44:23 +0100

    Add Themes

---
+++ config/scripts/themes/Horizon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp ~/.config/scripts/themes/Horizon/dunstrc ~/.config/dunst/
+
+cp ~/.config/scripts/themes/Horizon/config.rasi ~/.config/rofi/
+
+cp ~/.config/scripts/themes/Horizon/colors.conf ~/.config/i3/
+
+cp ~/.config/scripts/themes/Horizon/alacritty.yml ~/.config/alacritty/
+
+cp ~/.config/scripts/themes/Horizon/config ~/.config/polybar/
+
+cp ~/.config/scripts/themes/Horizon/settings.ini ~/.config/gtk-3.0/
+cp ~/.config/scripts/themes/Horizon/.gtkrc-2.0 ~/
---
+++ config/scripts/themes/Pink.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp ~/.config/scripts/themes/Pink/dunstrc ~/.config/dunst/
+
+cp ~/.config/scripts/themes/Pink/config.rasi ~/.config/rofi/
+
+cp ~/.config/scripts/themes/Pink/colors.conf ~/.config/i3/
+
+cp ~/.config/scripts/themes/Pink/alacritty.yml ~/.config/alacritty/
+
+cp ~/.config/scripts/themes/Pink/config ~/.config/polybar/
+
+cp ~/.config/scripts/themes/Pink/settings.ini ~/.config/gtk-3.0/
+cp ~/.config/scripts/themes/Pink/.gtkrc-2.0 ~/
